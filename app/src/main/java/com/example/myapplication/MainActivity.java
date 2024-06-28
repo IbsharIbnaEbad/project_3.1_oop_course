395acba6b603f869ab4d0797aeb5a8639202c787
ibshar <cse_182210012101018@lus.ac.bd>
2024-06-29 01:46:28 +0600

    making log in and register page active on click of btn and xml design

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +20,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+       /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            return insets;*/
+
+
+
+        Button btnreg, btnlog;
+      //  edtid = findViewById(R.id.edtid);
+        //edtpass = findViewById(R.id.edtpass);
+        btnlog = findViewById(R.id.btnlog);
+        btnreg = findViewById(R.id.btnreg);
+
+
+        btnlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // btn e click korle jekta oibo ogu ono lekmu
+                // bmi t ase kemne amra fetch kormu data xml taki
+            }
         });
-    }
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inext;//1
+                inext = new Intent (MainActivity.this, register.class);//2
+                //1 2 line jekuno jaygay dewa jay doesnt matter but start activy btn click er bitore likhte hoy
+
+                startActivity(inext);
+            }
+            // text dekaimu time faile log in as user dia id dimu
+        });
+
+
+}
 }
